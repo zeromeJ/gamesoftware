ee414c836eeb7745f463ddd6ac6cbf999de2571c
김상협 <dudwogkrrmq@gmail.com>
2024-11-10 23:12:46 +0900

    Init - 해상도변경 - 배경, 캐릭터 추가 - 캐릭터 임의 스프라이트 추가 - 점프 구현
    해상도 - 21:9

---
+++ Assets/Megu/Script/CenterCharacter.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterCharacter : MonoBehaviour
+{
+    public float jumpPower = 12.0f;
+    private Rigidbody2D rigidBody;
+    private Animator animator;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 일단은 스페이스바로 점프
+        if(Input.GetKeyDown(KeyCode.Space) && !animator.GetBool("IsJump"))
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Landing Platform
+        if(rigidBody.velocity.y < 0)
+        {
+            Debug.DrawRay(rigidBody.position, Vector3.down, Color.red);
+            RaycastHit2D raycastHit = Physics2D.Raycast(rigidBody.position, Vector2.down, 1.0f, LayerMask.GetMask("Platform"));
+            if(raycastHit.collider != null )
+            {
+                if (raycastHit.distance < 1.0f)
+                {   
+                    animator.SetBool("IsJump", false);
+                }
+            }
+        }
+    }
+
+    private void Jump()
+    {
+        rigidBody.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        animator.SetBool("IsJump", true);
+    }
+}
