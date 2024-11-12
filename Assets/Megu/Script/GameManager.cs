131d3d65f1ca48726764c7a3a49ca3aab2cd6bf4
김상협 <dudwogkrrmq@gmail.com>
2024-11-13 03:29:05 +0900

    한바퀴 돌았을 때 판정, 게임 매니저 추가

---
+++ Assets/Megu/Script/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Rope;
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RopeEnter()
+    {
+        Debug.Log("RopeEnter");
+    }
+
+    public void RopeExit()
+    {
+        Debug.Log("RopeExit");
+    }
+}
--- Assets/Megu/Script/Rope.cs
+++ Assets/Megu/Script/Rope.cs
@@ -7,18 +7,48 @@ public class Rope : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
     private Animator animator;
-    private Collider2D collideCheck;
+    private BoxCollider2D collideCheck;
     public float ropeSpeed = 0.5f;
-    private float currentSpeed;
     private bool isReversing = false;
+    private bool playerHit = false;
+    private bool dummyHit = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 더미가 먼저 맞는 현상 있음 어캐 수정하지
+        //if (collision.gameObject.name == "Player")
+        //{
+        //    // 플레이어가 줄에 걸렸을 때 로직 구현하기
+        //    Debug.Log("Player Hit! Game End");
+        //    return;
+        //}
+        //else
+        //{
+        //    // 더미가 맞았을 때 (줄넘기를 넘었으니 점수 획득 로직)
+        //    Debug.Log(collision.gameObject);
+        //}
+        // 플레이어가 먼저 충돌하는지 확인
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log(collision.gameObject);
+            playerHit = true;
+            return; // 플레이어가 감지되면 종료
+        }
+
+        // 플레이어가 감지되지 않은 경우 더미 처리
+        if (collision.CompareTag("Dummy"))
+        {
+            // 더미가 맞았을 경우 처리
+            Debug.Log(collision.gameObject);
+            dummyHit = true;
+        }
+    }
 
     void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
-        collideCheck = GetComponent<Collider2D>();
-        currentSpeed = ropeSpeed;
-        Debug.Log("Rope Setup");
+        collideCheck = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -48,22 +78,42 @@ public class Rope : MonoBehaviour
             }
         }
 
+       
+        
     }
+    private void LateUpdate()
+    {
+        if (dummyHit && !playerHit)
+        {
+            // 점수 증가 로직
+            IncreaseScore();
+            dummyHit = false; // 플래그 초기화
+        }
 
+        // 플레이어가 히트된 경우 게임 오버 처리
+        if (playerHit)
+        {
+            // 게임 오버 처리 로직
+            //HandleGameOver();
+            Debug.Log("GameOver");
+            playerHit = false; // 플래그 초기화
+        }
+
+    }
     private void FixedUpdate()
     {
-        if(spriteRenderer.sprite.name == "rope_2" && isReversing)
+        if (spriteRenderer.sprite.name == "rope_2" && isReversing)
         {
             collideCheck.enabled = true;
         }
-        else 
+        else
         {
             collideCheck.enabled = false;
         }
     }
 
-    //private void OnCollisionEnter(UnityEngine.Collision collision)
-    //{
-        
-    //}
+    private void IncreaseScore()
+    {
+        Debug.Log("점수 +1");
+    }
 }
